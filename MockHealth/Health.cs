701e8d4b360e27dabef20ec1e5a7dba3c0f424a9
Yusuf Emritte <34180742+ysfemritte@users.noreply.github.com>
2021-09-17 18:33:27 +0400

    Add files via upload

---
+++ MockHealth/Health.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockHealth
+{
+    class Health
+    {
+        //1. Create Instance Variable - IV (PRIVATE & LOWER CASE) --> Needs to be implemented in 3. Implemented Properties
+        private int birthYear;
+        private int weight;
+        private double height;
+
+        //2. Auto-Implemented Properties --> No need to validate - String DType
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //3. Implemented Properties
+        public int BirthYear
+        {
+            get
+            {
+                return birthYear;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    birthYear = value;
+                }
+            }
+
+        }// end of BirthYear
+
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    weight = value;
+                }
+            }
+        }// end of Weight
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    height = value;
+                }
+            }
+        }// end of Height
+
+
+        //4. Parameterless Constructor
+
+        //5. Parameterised Constructor
+        public Health(string _firstName, string _lastName, int _birthYear, int _weight, double _height)
+        {
+            //Properties = Parameters
+            FirstName = _firstName;
+            LastName = _lastName;
+            BirthYear = _birthYear;
+            Weight = _weight;
+            Height = _height;
+        }
+
+        //6. Methods
+
+
+        /// <summary>
+        /// Purpose: A method/property called "Age"
+        /// </summary>
+        /// <returns>Returns the person's age based on birthYear</returns>
+        public int Age()
+        {
+            int age;
+            age = 2021 - birthYear;
+            return age;
+        }
+
+        /// <summary>
+        /// Purpose: A Method/Property called "MaxHeartRate"
+        /// </summary>
+        /// <returns>Returns the person's maximum heart rate</returns>
+        public int MaxHeartRate()
+        {
+            int maxHeartRate;
+            maxHeartRate = 220 - Age();
+            return maxHeartRate;
+        }
+
+        /// <summary>
+        /// Purpose: A Method called BMI
+        /// </summary>
+        /// <returns>Returns a string that includes BMI and BMI Range</returns>
+        public double BMI()
+        {
+            double bmi;
+            bmi = (Weight / (Height * Height));
+            
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("Underweight");
+            }
+            else if (bmi >=18.5 && bmi <25)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (bmi >= 25 && bmi < 30)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else
+            {
+                Console.WriteLine("Obese");
+            }
+            
+            return bmi;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Full Name: {FirstName} {LastName} \tBirth year: {BirthYear} \tWeight: {Weight}" +
+                $"\tHeight: {Height} \tAge: {Age()} \tMaximum Heart Rate {MaxHeartRate()} \tBMI: {BMI()}");
+        }
+
+
+    }// End of Class - NO CODES AFTER THIS CURLY BRACKET
+}// End of Namespace
---
+++ MockHealth/TestProgram.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MockHealth
+{
+    class TestProgram
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //1. Create 2 objects of Health
+                //ClassName objName = new Constructor(parameter list)
+                Health objTest1 = new Health("John", "Smith", 1999, 60, 1.70);
+                Health objTest2 = new Health("Sarah", "Doe", 2002, 45, 1.59);
+
+                //2. Create an array of appropriate size and add the health objects to the array
+                //string[] HealthObjectArray = new string[10];
+                
+
+                //3.  Display the details of each object using the DisplayDetails method
+                objTest1.DisplayDetails();
+                Console.WriteLine("");
+                objTest2.DisplayDetails();
+                Console.WriteLine("");
+
+                //4. For the 1st object created, call the Age method/property and display the age of the person
+                Console.WriteLine("The Age of Person 1 is: " + objTest1.Age().ToString());
+                Console.WriteLine("");
+
+                //5. For the 2nd object created call the MaxHeartRate and diaplay the details
+                Console.WriteLine("The Maximum Heart Rate of Person 2 is: " + objTest2.MaxHeartRate().ToString());
+                Console.WriteLine("");
+
+                //6. For the 1st object, call the BMI Method and display the output
+                Console.WriteLine("The BMI of Person 1 is: " + objTest1.BMI().ToString());
+                Console.WriteLine("");
+
+                //7. For the 2nd object, use the appropriate array index and call the Display Method
+
+                //8. For the 2nd object, change the weight and height to different values
+                objTest2.Weight = 42;
+                objTest2.Height = 1.60;
+                Console.WriteLine($"Updated weight: {objTest2.Weight}, \nUpdated height {objTest2.Height}");
+                Console.WriteLine("");
+
+                //9. For the 2nd object, call the BMI method to calculate the output
+                Console.WriteLine("The BMI of Person 2 is: " + objTest2.BMI().ToString());                
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }// End Main
+    }
+}
